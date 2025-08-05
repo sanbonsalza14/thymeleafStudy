0c24734d3ff7872fd19c25ecadda9b0257b05990
804-03 <slamguy14@naver.com>
2025-08-05 17:22:30 +0900

    query string

--- src/main/java/com/my/thymeleftTest/controller/MainController.java
+++ src/main/java/com/my/thymeleftTest/controller/MainController.java
@@ -1,7 +1,11 @@
 package com.my.thymeleftTest.controller;
 
+import com.my.thymeleftTest.dto.IveMember;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.lang.reflect.Member;
 
 @Controller
 public class MainController {
@@ -13,8 +17,15 @@ public class MainController {
 
     }
 
+    @GetMapping("/templates/link")
+    public String linkPage() {
+        return "mainPage";
+    }
+
     @GetMapping("/link")
-    public String linkPage(){
-        return  "mainPage";
+    public String linkPage(Model model) {
+        IveMember member = new IveMember("장원영", 21, "서울");
+        model.addAttribute("jang", member);
+        return "/link/link_test";
     }
 }
